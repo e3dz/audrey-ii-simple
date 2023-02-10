dc69f296a8d801d8c81e1eeb0bfb12991002742a
Nick Donaldson <ndonald2@gmail.com>
2023-02-10 15:09:47 -0700

    Add allocate_buf to SDRAM allocation methods

--- Source/memory/sdram_alloc.h
+++ Source/memory/sdram_alloc.h
@@ -2,26 +2,27 @@
 #ifndef INFS_SDRAM_ALLOC_H
 #define INFS_SDRAM_ALLOC_H
 
+#include <stddef.h>
 #include <atomic>
 #include <new>
 
 namespace infrasonic
 {
     /**
-     * @brief 
+     * @brief
      * Provides an interface for monotonic, pseudo-dynamic allocation of memory
      * into Daisy SDRAM.
-     * 
+     *
      * @details
      * By default statically reserves 32MB of storage in SDRAM for runtime allocation.
      * Due to Daisy's STM32 lacking an MMU, please note that "dynamic" allocations
      * made this way will never actually be reclaimed during program lifetime.
-     * 
-     * @warning 
+     *
+     * @warning
      * This only works when built on Daisy platform and does not (yet?)
      * handle allocations beyond the max available memory with anything more
      * than a breakpoint.
-     * 
+     *
      */
     class SDRAM {
 
@@ -31,6 +32,14 @@ namespace infrasonic
             {
                 auto ptr = instance().allocate_raw(sizeof(T), alignof(T));
                 return new (ptr) T(std::forward<Args>(args)...);
+            }
+
+            // NOTE: UNTESTED
+            template<typename T>
+            static T* allocate_buf(size_t size)
+            {
+                auto ptr = instance().allocate_raw(size * sizeof(T), alignof(T));
+                return reinterpret_cast<T*>(ptr);
             }
 
         private:
@@ -46,7 +55,7 @@ namespace infrasonic
 
             std::atomic_size_t pool_pos_ = {0};
     };
-    
+
 } // namespace infrasonic
 
 #endif
